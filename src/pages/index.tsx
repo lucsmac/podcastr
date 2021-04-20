cc2865dd3a7e0733e35cf2d177b5dc11728698c6
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-20 18:06:19 -0300

    fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,3 +13,15 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const episodes = await response.json()
+
+  return {
+    props: {
+      episodes
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
